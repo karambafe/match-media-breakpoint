e073d98ccbca5e0fda7896ac525c7fcae320f377
Vitaly Blokhin <karambafe@yahoo.com>
2019-06-10 09:27:19 +0300

    feat: update dependencies

--- rollup.demo.config.js
+++ rollup.demo.config.js
@@ -1,11 +1,13 @@
 import vue from 'rollup-plugin-vue';
 import uglify from 'rollup-plugin-uglify-es';
 import html from 'rollup-plugin-fill-html';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'docs-src/main.js',
-  dest: 'docs/bundle-[hash].js',
+  output: 'docs/bundle-[hash].js',
   plugins: [
+    commonjs(),
     vue({
       css: true,
       compileTemplate: true,
